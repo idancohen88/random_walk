c7731f4bb0ed19880abaaf53783e458d179a899f
idan cohen private <idancohen88@gmail.com>
2021-01-09 13:08:01 +0200

    tree lean investigation

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+
+os.environ["PURE_PYTHON"] = "True"
+from BTrees import check
+from BTrees.OOBTree import OOBTree
+
 from btree_ext_lean import OOBTreeExtLean
 from build_tree import generate_btree_index_x_values_with_dist
 
@@ -8,8 +14,12 @@ prefix_to_percent = {
     'rrrr': 0.03,
     '': 0.47
 }
-num_of_values = 500_000
+num_of_values = 4_000_000
 my_index = generate_btree_index_x_values_with_dist(num_of_values, prefix_to_percent, OOBTreeExtLean())
+
+#my_index_2 = OOBTree()
+#my_index_2.update(my_index.items())
+
 while (True):
     my_index.random_sampling(k=100)
 
