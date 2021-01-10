442302de042652089f07890e499274b093263aa2
idan cohen private <idancohen88@gmail.com>
2021-01-10 18:13:25 +0200

    version of accepting following ac/rj

--- join/join_sampling.py
+++ join/join_sampling.py
@@ -26,45 +26,29 @@ def _sampled_element_is_valid(element, curr_table, sampled_elements, other_table
 
 
 
-def _sample(tbl1, tbl2):
-    tbl1_participant = sum([tbl2.freq[x] for x in set(tbl1.keys)])
-    tbl2_participant = sum([tbl1.freq[x] for x in set(tbl2.keys)])
+def _sample(tbl1, tbl2, k=50):
     chosen_elements = defaultdict(lambda: {'join_freq': 0, 'sampled': 0, 'sampled_from': [], 'iteration': 0})
     freq_in_join_estimate = {key: value * tbl2.freq[key] for key, value in tbl1.freq.items() if value * tbl2.freq[key]}
-    #freq_in_join_estimate_per_tbl = {key: value * tbl2.freq[key] for key, value in tbl1.freq.items() if value * tbl2.freq[key]}
+    sum_of_join = sum(freq_in_join_estimate.values())
 
-    sampled = 0
-    i = 0
-    K = 100
+    rejects = []
 
-    samples = []
-    full_join = tbl1.join(tbl2)
-    print('we have full join')
-    values_in_join = {x[0] for x in full_join}
-    tables_picker = _tables_iterator(tbl1, tbl2)
-    sampled_elements = []
-    declined = []
-    while sum([x['sampled'] for x in chosen_elements.values()]) < K:
-        i += 1
-        curr_table, other_table = next(tables_picker)
-
-        print('curr table selected')
-        element = curr_table.rand()
+    while sum([x['sampled'] for x in chosen_elements.values()]) < k:
+        element = tbl1.rand()
         element_key = element[0]
 
-        while not _sampled_element_is_valid(element, curr_table, sampled_elements, other_table):
-            declined.append(element)
-            element = curr_table.rand()
-            element_key = element[0]
+        accept_prob = freq_in_join_estimate.get(element_key, 0 )/ sum_of_join
+        accept_test_pass = np.random.random_sample() <= accept_prob
 
-        print('we have curr table')
+        if not accept_test_pass:
+            rejects.append(element_key)
+            continue
+
         chosen_elements[element_key]['join_freq'] = freq_in_join_estimate[element_key]
 
 
         chosen_elements[element_key]['sampled'] += 1
-        chosen_elements[element_key]['sampled_from'].append(curr_table.name)
-        sampled_elements.append((element, curr_table.name))
-        print(f'sampled element {element} from table {curr_table.name}')
+
 
     print('done sampling')
 
