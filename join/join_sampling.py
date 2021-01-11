9cc70936147409d588c2b33bfdec5b258da42e83
idan cohen private <idancohen88@gmail.com>
2021-01-11 09:59:07 +0200

    two stratergies, Chaunduri and round robin

--- join/join_sampling.py
+++ join/join_sampling.py
@@ -78,7 +78,53 @@ def _extract_k_candidate_tbl1(k, tbl1, tbl2):
     return [tbl1.weighted_rand(tbl2.freq) for _ in range(k)]
 
 
+def _sampled_element_is_valid(element, curr_table, sampled_elements, other_table):
+    element_key = element[0]
+    specific_element_sampled = sum([sampled_element == element and table_name==curr_table.name for sampled_element, table_name in sampled_elements])
+
+    max_sampling_allowed_for_element_from_curr_table = other_table.freq[element_key]
+    return specific_element_sampled < max_sampling_allowed_for_element_from_curr_table
+
+
+
 def _sample(tbl1, tbl2, k=50):
+    full_join = tbl1.join(tbl2)
+    chosen_elements = defaultdict(lambda: {'join_freq': 0, 'sampled': 0, 'sampled_from': [], 'iteration': 0})
+    freq_in_join_estimate = {key: value * tbl2.freq[key] for key, value in tbl1.freq.items() if value * tbl2.freq[key]}
+
+    #
+    i = 0
+
+    tables_picker = _tables_iterator(tbl1, tbl2)
+    sampled_elements = []
+    declined = []
+
+    while sum([x['sampled'] for x in chosen_elements.values()]) < k:
+        i += 1
+        curr_table, other_table = next(tables_picker)
+
+        print('curr table selected')
+        element = curr_table.rand()
+        element_key = element[0]
+
+        while not _sampled_element_is_valid(element, curr_table, sampled_elements, other_table):
+            declined.append(element)
+            element = curr_table.rand()
+            element_key = element[0]
+
+        print('we have curr table')
+        chosen_elements[element_key]['join_freq'] = freq_in_join_estimate[element_key]
+
+
+        chosen_elements[element_key]['sampled'] += 1
+        chosen_elements[element_key]['sampled_from'].append(curr_table.name)
+        sampled_elements.append((element, curr_table.name))
+        print(f'sampled element {element} from table {curr_table.name}')
+
+    print('done sampling')
+
+
+def _sample2(tbl1, tbl2, k=50):
     chosen_elements = defaultdict(lambda: {'join_freq': 0, 'sampled': 0, 'sampled_from': [], 'iteration': 0})
     freq_in_join_estimate = {key: value * tbl2.freq[key] for key, value in tbl1.freq.items() if value * tbl2.freq[key]}
     sum_of_join = sum(freq_in_join_estimate.values())
@@ -115,7 +161,7 @@ def main():
     tbl2 = Table('tbl2', [random.randint(10, 20) for _ in range(53)])
 
 
-    _sample2(tbl1, tbl2)
+    _sample(tbl1, tbl2)
 
 if __name__ == '__main__':
     main()
