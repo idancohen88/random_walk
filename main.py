9f2e5b96d3a07d08e994fcb6bbb2c5ad5c9d1555
idan cohen private <idancohen88@gmail.com>
2021-03-13 16:40:22 +0200

    organize directories

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 os.environ["PURE_PYTHON"] = "True"
 
 from btrees.btree_ext import OOBTreeExt, WALKING_TECHNIQUE
-from btrees.build_tree import generate_btree_index_x_values_with_dist
+from build_tree.build_tree import generate_btree_index_x_values_with_dist
 
 prefix_to_percent = {
     'gggg': 0.25,
